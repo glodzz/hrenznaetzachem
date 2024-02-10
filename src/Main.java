e8acbb4e9071a6444269625345e22399c3c406d4
JustPlayerFire <timurdauto8@mail.ru>
2024-02-10 15:49:26 +0300

    init 3

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello again!");//maybe the problem is in that code!?
+        //No, I've found the problem in system files
     }
 }
